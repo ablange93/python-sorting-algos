c573f7f68260d271912663c3c7ccdd2e63f9ca00
Andrew Lange <andrew.lange93@gmail.com>
2018-11-04 19:59:06 -0500

    Quicksort: Implemented base case and pivot variable definition.

--- quicksort.py
+++ quicksort.py
@@ -1,9 +1,14 @@
 def quicksort(list, start, end):
-  # BASE CASE
+  # BASE CASE #
   if start >= end:
     return
   
-  # INDUCTIVE STEP
+  # Define pivot variable and swap with last element
+  pivot_idx = randrange(start, end)
+  pivot_element = list[pivot_idx]
+  list[end], list[pivot_idx] = list[pivot_idx], list[end]
+  
+  # INDUCTIVE STEP #
   print(list[start])
   start += 1
   quicksort(list, start, end)
